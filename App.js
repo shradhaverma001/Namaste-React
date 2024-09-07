ed7833d79d376c35d4a18ffb5baa89cef3286c1a
Shradha Verma <shradhaverma001@gmail.com>
2024-09-07 12:58:25 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,35 @@
+// {} is an obeject which represents attributes of the element.
+// createElement basically returns a JavaScript object.
+
+/*
+ CREATE THIS STRUCTURE USING REACT ELEMENT
+ <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+ </div>
+
+ React.createElement(Object) => HTML(Browser Understands)
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from react!");
+console.log(parent); //returns an object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
