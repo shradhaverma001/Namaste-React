72dab76c2edcd5ff2651215e9c91556dd3db1f68
Shradha Verma <shradhaverma001@gmail.com>
2024-09-17 19:46:48 +0530

    till episode 3

--- App.js
+++ App.js
@@ -1,35 +1,56 @@
-// {} is an obeject which represents attributes of the element.
-// createElement basically returns a JavaScript object.
+import React from "react";
+import ReactDOM from "react-dom";
 
-/*
- CREATE THIS STRUCTURE USING REACT ELEMENT
- <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
+// React Element
+const jsxHeading = (
+    <h1 id="heading" className="head" tabIndex="5">Namaste react from JSX❤️</h1>
+);
+console.log(jsxHeading);
+
+// React functional component syntaxes
+const HeadingComponent1 = () => {
+    return <h1 className="heading">Namaste Javascript from functional component1</h1>
+};
+
+const number = 5000;
+const HeadingComponent2 = () => (
+    <div id="container">
+        {jsxHeading}
+        {1000}
+        {number}
+        {HeadingComponent1()}       
+        <HeadingComponent1/>
+        <h1 className="">Namaste Javascript from functional component🚀</h1>
     </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
- </div>
-
- React.createElement(Object) => HTML(Browser Understands)
-*/
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
-
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from react!");
-console.log(parent); //returns an object
+);
+// Both the above syntax are same it is returning a JSX.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent2 />);
+
+
+
+
+
+// PRACTICE
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const ele = (
+//     <h1> react Element</h1>
+// );
+
+// const Component1= () => (
+//         <h1 id="heading">react component1</h1>
+// );
+
+// const Component2 = () => (
+//     <div>
+//         <Component1 />
+//         <h2> after rendering component1 its comp2</h2>
+//     </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Component2 />);
