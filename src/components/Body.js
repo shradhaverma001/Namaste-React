a439442b0d381755b54640a1c4a23da2c2818dc2
Shradha Verma <shradhaverma001@gmail.com>
2024-10-02 14:59:19 +0530

    use effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react"; 
+// import resList from "../utils/mockData";
+import { useEffect, useState } from "react"; 
 
 const Body = () => {
 
   // State variable using hooks
-  let [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+    let [listOfRestaurants, setlistOfRestaurants] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await(data.json());
+        console.log(json);
+        setlistOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
 
 //   // Normal JS Variable
 //   let listOfRestaurants = [
@@ -60,6 +74,10 @@ const Body = () => {
 //       }
 //     },
 // ];
+
+    if(listOfRestaurants.length === 0){
+        return <h1>Loading....</h1>
+    }
 
     return (
         <div className="body">
